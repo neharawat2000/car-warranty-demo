34e42ffe6fcb3d31444fdc1d882f8cd31b793874
@princech11 <prince11@fusiontecz.com>
2025-10-13 21:15:59 +0530

    added second gtag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -23,6 +23,24 @@ export default function GoogleAnalytics() {
                     `,
                 }}
             />
+            {/* Google tag (gtag.js) */}
+            <Script
+                async
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-YX05N3MGFH"
+            />
+            <Script
+                id="google-analytics2"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-YX05N3MGFH');
+                    `,
+                }}
+            />
         </>
     );
 }
